6a0e7c88ee4fe61ab77c796e67c984178fadfad1
DumplingSociety <jobs4ever@me.com>
2019-02-12 13:35:22 -0600

    Create myTest.js

---
+++ myTest.js
@@ -0,0 +1,20 @@
+myFunction([
+{
+"display": "HTML Tutorial"
+},
+{
+"display": "CSS Tutorial"
+},
+{
+"display": "JavaScript Tutorial"
+},
+{
+"display": "SQL Tutorial"
+},
+{
+"display": "PHP Tutorial"
+},
+{
+"display": "XML Tutorial"
+}
+])
